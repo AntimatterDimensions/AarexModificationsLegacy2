b3434730c162d857da588d16d2f02a1a2c8c3c67
AntimatterDimensions <71886513+AntimatterDimensions@users.noreply.github.com>
2020-10-05 16:36:49 -0400

    Update achieve_check.js

--- javascripts/core/ach/achieve_check.js
+++ javascripts/core/ach/achieve_check.js
@@ -279,7 +279,7 @@ function ngP3AchieveCheck(){
 	if (speedrunMilestonesReached >= 28) giveAchievement("Special Relativity")
 	if (tmp.qu.best <= 10) giveAchievement("Quantum doesn't take so long")
 	if (player.masterystudies.includes("d13")) giveAchievement("Do protons decay?")
-	if (getTotalRadioactiveDecays >= 10) giveAchievement("Radioactive Decaying to the max!")
+	if (getTotalRadioactiveDecays() >= 10) giveAchievement("Radioactive Decaying to the max!")
 	if (quantumed) giveAchievement("Sub-atomic")
 
 	if (tmp.ngp3l) return // NG+3.1 achievements from this point on
